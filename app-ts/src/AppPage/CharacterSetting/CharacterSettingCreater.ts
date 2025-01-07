c59f888cdc586d4cc3b4807f935a8c779b71aa2a
unknown <poke301qup@gmail.com>
2025-01-08 03:40:49 +0900

    VoiceVoxの音声設定モデルを追加し、関連するクラスと型を更新しました。CevioAIの設定をVoiceVoxに統合しました。

--- app-ts/src/AppPage/CharacterSetting/CharacterSettingCreater.ts
+++ app-ts/src/AppPage/CharacterSetting/CharacterSettingCreater.ts
@@ -1,10 +1,14 @@
 import { IOpenCloseWindow } from "../../UiComponent/Board/IOpenCloseWindow";
 import { TTSSoftware } from "../../ValueObject/Character";
 import { createCevioAIVoiceSetting } from "./CevioAIVoiceSetting";
+import { createVoiceVoxVoiceSetting } from "./VoiceVoxVoiceSetting";
 
 export function createCharacterVoiceSetting(character_id: string, ttsSoftWare: TTSSoftware): IOpenCloseWindow|null {
     if (ttsSoftWare === "CevioAI") {
         return createCevioAIVoiceSetting(character_id);
+    } 
+    else if (ttsSoftWare === "VoiceVox") {
+        return createVoiceVoxVoiceSetting(character_id);
     }
     return null;
 }
--- app-ts/src/ZodObject/DataStore/ChatacterVoiceSetting/VoiceVoxVoiceSetting/VoiceVoxVoiceSettingModel.ts
+++ app-ts/src/ZodObject/DataStore/ChatacterVoiceSetting/VoiceVoxVoiceSetting/VoiceVoxVoiceSettingModel.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
-export const SettingForQuery = z.object({
+export const VoiceVoxVoiceSettingModel = z.object({
   speedScale: z.number().gte(0.5).lte(2).default(1),
   pitchScale: z.number().gte(-0.15).lte(0.15).default(0),
   intonationScale: z.number().gte(0).lte(2).default(1),
   volumeScale: z.number().gte(0).lte(2).default(1),
 });
-export type SettingForQuery = z.infer<typeof SettingForQuery>;
+export type VoiceVoxVoiceSettingModel = z.infer<
+  typeof VoiceVoxVoiceSettingModel
+>;
