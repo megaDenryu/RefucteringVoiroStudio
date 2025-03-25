634bfe61039837d6c16dec4dec6716ef9a701684
megaDenryu <poke301qup@gmail.com>
2025-03-25 20:25:28 +0900

    方針策定機能を追加し、思考グラフのクラス名を変更。プロセス計画のLLM用途タイプを更新。

--- api/LLM/エージェント/会話用エージェント/自立型Ver1/体/BrainModule/脳内思考プロセス/脳内思考プロセス.py
+++ api/LLM/エージェント/会話用エージェント/自立型Ver1/体/BrainModule/脳内思考プロセス/脳内思考プロセス.py
@@ -9,7 +9,7 @@ from api.LLM.LLMAPIBase.切り替え可能LLM import 切り替え可能LLMBox
 from api.LLM.LLMAPIBase.切り替え可能LLMファクトリーリポジトリ import 切り替え可能LLMファクトリーリポジトリ
 from api.LLM.エージェント.会話用エージェント.自立型Ver1.会話履歴.ValueObject.MessageUnit import MessageUnitVer1
 from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.プロセス材料.プロセス材料を包んだもの import プロセス材料を包んだもの
-from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.思考グラフ.思考グラフ import 思考グラフ
+from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.思考グラフ.思考グラフ部署 import 思考グラフ部署
 from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.思考プロセス状態 import 思考状態
 from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.状況統合.状況オブジェクト import 状況
 from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.状況統合.状況統合所 import 状況統合所
@@ -21,7 +21,7 @@ class 脳内思考プロセス:
     _v自分の情報:I自分の情報コンテナ
     _思考履歴: list[思考状態]
     _外部状況統合所: 状況統合所
-    _思考グラフ: 思考グラフ
+    _思考グラフ部署: 思考グラフ部署
     _プロセス材料溜め置き場: list[プロセス材料を包んだもの]
     _llmプロセス計画:切り替え可能LLMBox
     @property
@@ -38,7 +38,7 @@ class 脳内思考プロセス:
         self._プロセス材料溜め置き場 = []
         self._llmプロセス計画 = 切り替え可能LLMファクトリーリポジトリ.singleton().getLLM(LLM用途タイプ.プロセス計画)
         self._外部状況統合所 = 状況統合所()
-        self._思考グラフ = 思考グラフ(self._v自分の情報)
+        self._思考グラフ部署 = 思考グラフ部署(self._v自分の情報)
 
     async def 脳内思考プロセスを開始(self):
         """
@@ -52,7 +52,7 @@ class 脳内思考プロセス:
         #     思考内容 = ConvertAndSaveLog.MarkdownConvertList(model = messageUnits, log=True)
         # )
         状況履歴:list[状況] = self._外部状況統合所.思考可能な形に状況を整理して変換(self._プロセス材料溜め置き場,最新メッセージ)
-        new思考状態:思考状態 = self._思考グラフ.思考を進める(状況履歴)
+        new思考状態:思考状態 = self._思考グラフ部署.思考を進める(状況履歴)
         self._思考履歴.append(new思考状態)
         self._プロセス材料溜め置き場 = []
     
