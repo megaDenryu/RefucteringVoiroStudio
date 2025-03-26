b57d2868d75a44145d03cfce9c6d16e5ec7647ac
megaDenryu <poke301qup@gmail.com>
2025-03-26 23:06:18 +0900

    Conversationクラスにコンストラクタを追加し、外部イベントクラスの継承を削除

--- api/LLM/エージェント/会話用エージェント/自立型Ver1/会話履歴/ValueObject/Conversation.py
+++ api/LLM/エージェント/会話用エージェント/自立型Ver1/会話履歴/ValueObject/Conversation.py
@@ -5,5 +5,7 @@ from api.LLM.エージェント.会話用エージェント.自立型Ver1.会話
 
 
 # 会話履歴。jsonとして保存することも視野に入れている。
-class Conversation(BaseModel):
+class Conversation:
     history: list[MessageUnitVer1]
+    def __init__(self, history: list[MessageUnitVer1]):
+        self.history = history
--- api/LLM/エージェント/会話用エージェント/自立型Ver1/外部イベント.py
+++ api/LLM/エージェント/会話用エージェント/自立型Ver1/外部イベント.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from api.LLM.エージェント.会話用エージェント.自立型Ver1.会話履歴.ValueObject.Conversation import Conversation
 
 
-class 外部イベント(BaseModel):
+class 外部イベント:
     会話:Conversation
-    
+    def __init__(self, 会話:Conversation):
+        self.会話 = 会話
