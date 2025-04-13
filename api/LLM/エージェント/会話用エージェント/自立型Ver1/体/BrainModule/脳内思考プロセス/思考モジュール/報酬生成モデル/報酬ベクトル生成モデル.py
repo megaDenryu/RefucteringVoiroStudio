9799564d7cac225f209bda8bb04f9f35bd184753
megaDenryu <poke301qup@gmail.com>
2025-04-14 06:05:03 +0900

    feat: 新しい報酬生成モデルと関連クラスを追加し、感情処理機能を強化

--- api/LLM/エージェント/会話用エージェント/自立型Ver1/体/BrainModule/脳内思考プロセス/思考モジュール/報酬生成モデル/報酬ベクトル生成モデル.py
+++ api/LLM/エージェント/会話用エージェント/自立型Ver1/体/BrainModule/脳内思考プロセス/思考モジュール/報酬生成モデル/報酬ベクトル生成モデル.py
@@ -2,8 +2,8 @@ from api.Extend.BaseModel.model_dumpable import IModelDumpAble
 from api.LLM.LLMAPIBase.LLM用途タイプ import LLMs用途タイプ
 from api.LLM.LLMAPIBase.切り替え可能LLM import 切り替え可能LLMBox
 from api.LLM.LLMAPIBase.切り替え可能LLMファクトリーリポジトリ import 切り替え可能LLMファクトリーリポジトリ
-from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.思考モジュール.感情ベクトル生成モデル.出力BaseModel import Benefit, EmotionVector
-from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.思考モジュール.感情ベクトル生成モデル.感情ベクトル生成クエリプロキシ import 感情ベクトル生成クエリプロキシ
+from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.思考モジュール.報酬生成モデル.出力BaseModel import Benefit, EmotionVector
+from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.思考モジュール.報酬生成モデル.報酬ベクトル生成クエリプロキシ import 感情ベクトル生成クエリプロキシ
 from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.思考履歴 import 思考履歴
 from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.状況統合.状況オブジェクト import 状況履歴
 
--- api/LLM/エージェント/会話用エージェント/自立型Ver1/体/BrainModule/脳内思考プロセス/思考モジュール/統合思考モデル/連鎖的思考モデル.py
+++ api/LLM/エージェント/会話用エージェント/自立型Ver1/体/BrainModule/脳内思考プロセス/思考モジュール/統合思考モデル/連鎖的思考モデル.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.思考モジュール.専門家が結論を出すモデル.専門家が結論を出すモデル import 専門家が結論を出すモデル
-from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.思考モジュール.感情ベクトル生成モデル.感情ベクトル生成モデル import 感情ベクトル生成モデル
+from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.思考モジュール.報酬生成モデル.報酬ベクトル生成モデル import 感情ベクトル生成モデル
 from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.思考モジュール.深層的自問自答モデル.出力BaseModel import InternalMonologue
 from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.思考モジュール.深層的自問自答モデル.自問自答実行モジュール import 自問自答モジュール
 from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.思考履歴 import 思考履歴
