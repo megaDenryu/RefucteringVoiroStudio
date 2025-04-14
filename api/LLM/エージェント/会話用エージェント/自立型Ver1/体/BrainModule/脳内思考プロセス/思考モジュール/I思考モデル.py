079e64a18a2c8c79a987dfb23c1273b0cfe9b02c
megaDenryu <poke301qup@gmail.com>
2025-04-14 21:40:35 +0900

    feat: I思考モデルの戻り値をBaseModelからIModelDumpAbleに変更し、思考ゴールグラフモデルを追加

--- api/LLM/エージェント/会話用エージェント/自立型Ver1/体/BrainModule/脳内思考プロセス/思考モジュール/I思考モデル.py
+++ api/LLM/エージェント/会話用エージェント/自立型Ver1/体/BrainModule/脳内思考プロセス/思考モジュール/I思考モデル.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 
 from pydantic import BaseModel
 
+from api.Extend.BaseModel.model_dumpable import IModelDumpAble
 from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.思考履歴 import 思考履歴
 from api.LLM.エージェント.会話用エージェント.自立型Ver1.体.BrainModule.脳内思考プロセス.状況統合.状況オブジェクト import 状況履歴
 from api.LLM.エージェント.会話用エージェント.自立型Ver1.体を持つ者.自分の情報.I自分の情報 import I自分の情報コンテナ
@@ -12,7 +13,7 @@ class I思考モデル(ABC):
     思考モデルのインターフェース
     """
     @abstractmethod
-    async def 実行(self, v状況履歴: 状況履歴, v思考履歴: 思考履歴, vキャラクター情報:I自分の情報コンテナ) -> BaseModel:
+    async def 実行(self, v状況履歴: 状況履歴, v思考履歴: 思考履歴, vキャラクター情報:I自分の情報コンテナ) -> IModelDumpAble:
         """
         思考を進める
         """
